a4c73b3f6339d972acd3a3431b237c388b3668b7
mauriciobomfim12 <mauriciobomfimm@gmail.com>
2023-11-23 16:17:49 -0300

    olamundo

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require("http");
+
+http.createServer((req, res) => {
+    console.log(`Received request for: ${req.url}`);
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    if (req.url === "/") {
+        console.log("Responding with 'Olá Mundo'");
+        res.write("<h1>Olá Mundo</h1>");
+    }
+
+    res.end();
+}).listen(3000, () => {
+    console.log("Server listening on port 3000");
+});
