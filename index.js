7ef407648827a79fe500c4a41916f9474560da53
mauriciobomfim12 <mauriciobomfimm@gmail.com>
2023-11-23 19:39:07 -0300

    olamundoo

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var http = require("http");
+const http = require("http");
 
 http.createServer((req, res) => {
     console.log(`Received request for: ${req.url}`);
-
     res.writeHead(200, { "Content-Type": "text/html" });
 
     if (req.url === "/") {
@@ -11,6 +10,6 @@ http.createServer((req, res) => {
     }
 
     res.end();
-}).listen(3000, () => {
+}).listen(3000, "0.0.0.0", () => {
     console.log("Server listening on port 3000");
 });
