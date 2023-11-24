558d52d5f2ee964ef85f6ba7ce30e680a9afb730
mauriciobomfim12 <mauriciobomfimm@gmail.com>
2023-11-24 08:31:11 -0300

    olamundooooooo

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const https = require('https');
-const fs = require('fs');
 
-const options = {
-  key: fs.readFileSync('C:\\Users\\Administrator\\Downloads\\backend\\backend\\node_modules\\node-gyp\\test\\fixtures\\server.key'),
-  cert: fs.readFileSync('C:\\Users\\Administrator\\Downloads\\backend\\backend\\node_modules\\node-gyp\\test\\fixtures\\server.crt'),
-};
-
-const server = https.createServer(options, (req, res) => {
-  // Lógica do seu servidor
-});
-
-// O segundo argumento para listen é o endereço IP ao qual você deseja vincular
-// Se você deseja ouvir em todas as interfaces, use '0.0.0.0'
-server.listen(3000, '0.0.0.0', () => {
+https.createServer((req, res) => {
+  if(req.url == "/"){
+    res.write("<h1>ola mundo</h1>")
+  }
+}).listen('0.0.0.0', () => {
   console.log('Servidor HTTPS está escutando na porta 3000 em todas as interfaces');
 });
